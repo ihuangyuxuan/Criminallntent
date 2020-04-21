45af00d3b3ff22f9717bbb51599edba05047ae83
ihuangyuxuan <zuiaixiaochao@gmail.com>
2020-04-21 15:04:36 +0800

    Initial commit

---
+++ app/src/main/java/com/hziee/huangyuxuan/criminallntent/CrimeActivity.java
@@ -0,0 +1,23 @@
+package com.hziee.huangyuxuan.criminallntent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class CrimeActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+
+    }
+}
